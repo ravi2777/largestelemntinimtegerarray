98c29223a76252ed9c9b1995d46982267cb717a5
hp <hp@RAVIBAGHEL>
2023-08-29 22:10:12 +0530

    Add project files.

---
+++ largestelemntinimtegerarray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 5, 2, 9, 1, 7 };
+        int largest = array[0];
+
+        foreach (int num in array)
+        {
+            if (num > largest)
+            {
+                largest = num;
+            }
+        }
+
+        Console.WriteLine("Largest element in the array: " + largest);
+    }
+}
